3db8493e5ea273358aa5c3c16d2a74e3c2dd29ad
hithesh svsk <hiteshsunkara@eworldes.com>
2023-11-05 22:29:14 +0530

    redux

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,17 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '@/styles/globals.css'
-import { Provider } from 'react-redux';
-import { createStore } from "redux"
-import { Toaster } from "react-hot-toast"
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@/styles/globals.css";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import { Toaster } from "react-hot-toast";
 import rootReducer from "@/Store";
 
-
-const Store = createStore(rootReducer)
+const Store = createStore(rootReducer);
 
 export default function App({ Component, pageProps }) {
   return (
     <Provider store={Store}>
       <Component {...pageProps} />
-      <Toaster
-        position="top-right"
-      />
+      <Toaster position="top-right" />
     </Provider>
-  )
+  );
 }
