23c3bd5be392c828f516d4cd5f3e062a4ef75ade
SVSKHD <8svskhd@gmail.com>
2024-02-03 23:41:17 +0530

    product images seo corrections

--- src/pageComponents/product/dynamicProduct.js
+++ src/pageComponents/product/dynamicProduct.js
@@ -25,7 +25,7 @@ const DynamicProduct = () => {
   const SeoData = {
     title: `Aquakart | Product - ${product?.title}`,
     description:`${product?.description}`,
-    image:`${product.photos[0].secure_url}`,
+    image:`${product.photos[0]?.secure_url}`,
     keywords:`Aquakart Products ${product?.keywords}`
   };
   return (
