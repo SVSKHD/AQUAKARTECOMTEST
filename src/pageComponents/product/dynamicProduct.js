5da6836e4cfc77ee78e195f3c3321e10276e3d9a
SVSKHD <8svskhd@gmail.com>
2024-02-10 20:08:52 +0530

    product

--- src/pageComponents/product/dynamicProduct.js
+++ src/pageComponents/product/dynamicProduct.js
@@ -5,10 +5,19 @@ import Image from "next/image";
 import LOGO from "../../assests/logo.png";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
-import { Badge, Spinner } from "react-bootstrap";
+import { Badge, Spinner, InputGroup, Button, Form } from "react-bootstrap";
+import {
+  FaPlus,
+  FaMinus,
+  FaHeart,
+  FaRegHeart,
+  FaCartPlus,
+} from "react-icons/fa";
+import AquaButton from "@/reusables/button";
 
 const DynamicProduct = () => {
   const [product, setProduct] = useState({});
+  const [quantity, setQuantity] = useState(0);
   const [loading, setLoading] = useState(false);
   const { getProductById } = AquaProductOperations();
   const router = useRouter();
@@ -42,7 +51,7 @@ const DynamicProduct = () => {
           </div>
         ) : (
           <div className="row mb-3">
-            <div className="col-4">
+            <div className="col-md-4 col-lg-4 col-xs-12 col-sm-12">
               {product?.photos ? (
                 <>
                   <AquaProductUnControlledCarousel
@@ -61,11 +70,38 @@ const DynamicProduct = () => {
                 />
               )}
             </div>
-            <div className="col-8">
+            <div className="col-md-8 col-xs-12 col-sm-12 col-lg-8">
               <h1 className="display-2 text-bold">{product?.title}</h1>
-              <h4 className="price product-price-align">
-                <Badge bg="success"> ₹{product?.price}</Badge>
-              </h4>
+              <span className="dynamic-product-price-brand">
+                <h4 className="price product-price-align">
+                  <Badge bg="success"> ₹{product?.price}</Badge>
+                </h4>
+                <h4 className="dynamic-product-brand">
+                  Brand : {product?.brand}
+                </h4>
+              </span>
+              <InputGroup className="mb-3 width-adjust">
+                <Button variant="outline-secondary">
+                  <FaMinus size={25} />
+                </Button>
+                <Form.Control
+                  aria-label="Example text with two button addons"
+                  className="text-center"
+                  value={quantity}
+                />
+                <Button variant="outline-secondary">
+                  <FaPlus size={25} />
+                </Button>
+              </InputGroup>
+              <div className="dynamic-product-cart-fav">
+                <AquaButton>
+                  <FaCartPlus size={25} />
+                </AquaButton>
+                <AquaButton>Checkout</AquaButton>
+                <AquaButton variant="normal">
+                  <FaRegHeart size={25} className="text-danger" />
+                </AquaButton>
+              </div>
               <hr />
               <h5 className="text-muted">{product?.description}</h5>
             </div>
