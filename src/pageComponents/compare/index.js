0ed21da7e0b9040ec523de98adcc4b17c12a4577
SVSKHD <8svskhd@gmail.com>
2024-02-10 20:29:10 +0530

    compare

--- src/pageComponents/compare/index.js
+++ src/pageComponents/compare/index.js
@@ -1,14 +1,26 @@
 import AquaLayout from "@/Layout/Layout";
 import AquaHeading from "@/reusables/heading";
+import AquaTabs from "@/reusables/tabs";
 
 const AquaComparePageComponent = () => {
   const SeoData = {
     title: "Aquakart | Compare",
   };
+  const compareTabs = [
+    {
+      title: "Wishlist",
+      component: <h1>wishlist</h1>,
+    },
+    {
+      title: "Compare",
+      component: <h1>Compare</h1>,
+    },
+  ];
   return (
     <>
       <AquaLayout seo={SeoData}>
         <AquaHeading level={1}>Hello Aquakart Compare</AquaHeading>
+        <AquaTabs tabs={compareTabs} />
       </AquaLayout>
     </>
   );
--- src/reusables/tabs.js
+++ src/reusables/tabs.js
@@ -1,26 +1,35 @@
-import { Card, Tabs, Tab } from "react-bootstrap";
-const AquaTabs = (tabs) => {
+import React from 'react';
+import { Card, Tabs, Tab } from 'react-bootstrap';
+
+const AquaTabs = ({ tabs }) => {
+  if (!Array.isArray(tabs) || !tabs.length) {
+    // Optionally handle the case where tabs are not provided or not in the expected format
+    return null;
+  }
+
   return (
-    <>
-      <Card>
-        <Card.Body>
-          <Tabs
-            defaultActiveKey="profile"
-            id="justify-tab-example"
-            className="mb-3 tabs-background"
-            justify
-          >
-            {tabs.map((r) => (
-              <>
-                <Tab eventKey="home" title={r?.title}>
-                  {r?.component}
-                </Tab>
-              </>
-            ))}
-          </Tabs>
-        </Card.Body>
-      </Card>
-    </>
+    <Card className='shadow-lg mb-3'>
+      <Card.Body>
+        <Tabs
+          defaultActiveKey={tabs[0]?.title || 'defaultKey'}
+          id="aqua-tabs"
+          className="mb-3 tabs-background"
+          justify
+        >
+          {tabs.map((tab, index) => (
+            <Tab
+              className='m-3'
+              key={tab.title || index} // Ensure each tab has a unique key
+              eventKey={tab.title || `tab-${index}`}
+              title={tab.title}
+            >
+              {tab.component}
+            </Tab>
+          ))}
+        </Tabs>
+      </Card.Body>
+    </Card>
   );
 };
+
 export default AquaTabs;
