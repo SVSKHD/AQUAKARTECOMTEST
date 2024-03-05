ea3211788b9f32eb3c97b7d83fe4f3f8ce2fd77f
SVSKHD <8svskhd@gmail.com>
2024-03-05 08:03:57 +0530

    fixes

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -7,50 +7,78 @@ import { Toaster } from "react-hot-toast";
 import rootReducer from "@/Store";
 import { useEffect } from "react";
 import { useRouter } from "next/router"; // Import useRouter
-
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 const Store = createStore(rootReducer);
 
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer);
+export const persistor = persistStore(store);
+
+
 export default function App({ Component, pageProps }) {
-  const router = useRouter(); // Use the useRouter hook
+  const router = useRouter();
 
   useEffect(() => {
-    // Load existing cart from localStorage
+    // Function to initialize gtag
+    function gtag() {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push(arguments);
+    }
 
-    // Google Analytics
+    // Check if script is already loaded
+    if (!window.gtag) {
+        // Create script element
+        const script = document.createElement("script");
+        script.src = `https://www.googletagmanager.com/gtag/js?id=G-FS41RRVRD4`;
+        script.async = true;
+        document.head.appendChild(script);
+
+        // Initialize dataLayer and configure gtag
+        script.onload = () => {
+            window.dataLayer = window.dataLayer || [];
+            function gtag() {
+                dataLayer.push(arguments);
+            }
+            window.gtag = gtag;
+            gtag('js', new Date());
+            gtag('config', 'G-FS41RRVRD4');
+        };
+    }
+
+    // Route change handler
     const handleRouteChange = (url) => {
-      window.gtag("config", "G-FS41RRVRD4", {
-        page_path: url,
-      });
+        window.gtag('config', 'G-FS41RRVRD4', {
+            page_path: url,
+        });
     };
 
     // Subscribe to route changes
-    router.events.on("routeChangeComplete", handleRouteChange);
-    // Cleanup subscription on unmount
+    router.events.on('routeChangeComplete', handleRouteChange);
+
+    // Cleanup on unmount
     return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
+        router.events.off('routeChangeComplete', handleRouteChange);
     };
-  }, [router.events]);
+}, [router.events]);
+
+
+
 
   return (
-    <Provider store={Store}>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
       <Component {...pageProps} />
       <Toaster position="top-right" />
-
-      {/* Google Analytics Script */}
-      <script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-FS41RRVRD4"
-      ></script>
-      <script
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-FS41RRVRD4');
-          `,
-        }}
-      />
+      </PersistGate>
     </Provider>
   );
 }
