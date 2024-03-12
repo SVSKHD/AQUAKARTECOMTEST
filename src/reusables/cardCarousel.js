e7a9df6749a0e2dabbc3b93ea25669e26cca50d1
SVSKHD <8svskhd@gmail.com>
2024-03-12 22:41:58 +0530

    carousel test

--- src/reusables/cardCarousel.js
+++ src/reusables/cardCarousel.js
@@ -24,18 +24,73 @@ const responsive = {
 const AquaCardMultiItemCarousel = (props) => {
   return (
     <Carousel
-      arrows={false}
-      infinite={true}
-      autoPlaySpeed={7000}
-      autoPlay={true}
-      swipeable={true}
-      ssr={true}
-      draggable={true}
-      responsive={responsive}
-      className="card-carousel"
-    >
-      {props.children}
-    </Carousel>
+  additionalTransfrom={0}
+  arrows
+  autoPlaySpeed={3000}
+  centerMode
+  className=""
+  containerClass="container"
+  dotListClass=""
+  draggable
+  focusOnSelect={false}
+  infinite
+  itemClass=""
+  keyBoardControl
+  minimumTouchDrag={80}
+  pauseOnHover
+  renderArrowsWhenDisabled={false}
+  renderButtonGroupOutside={false}
+  renderDotsOutside={false}
+  responsive={{
+    desktop: {
+      breakpoint: {
+        max: 3000,
+        min: 1024
+      },
+      items: 3,
+      partialVisibilityGutter: 40
+    },
+    mobile: {
+      breakpoint: {
+        max: 464,
+        min: 0
+      },
+      items: 1,
+      partialVisibilityGutter: 25
+    },
+    tablet: {
+      breakpoint: {
+        max: 1024,
+        min: 464
+      },
+      items: 1,
+      partialVisibilityGutter: 30
+    }
+  }}
+  rewind={false}
+  rewindWithAnimation={false}
+  rtl={false}
+  shouldResetAutoplay
+  showDots={false}
+  sliderClass=""
+  slidesToSlide={1}
+  swipeable
+>
+{props.children}
+</Carousel>
+    // <Carousel
+    //   arrows={false}
+    //   infinite={true}
+    //   autoPlaySpeed={7000}
+    //   autoPlay={true}
+    //   swipeable={true}
+    //   ssr={true}
+    //   draggable={true}
+    //   responsive={responsive}
+    //   className="card-carousel"
+    // >
+    //   {props.children}
+    // </Carousel>
   );
 };
 
