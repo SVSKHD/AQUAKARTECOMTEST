aa36e8a41d50eb21d134a538f092e42032920a4a
SVSKHD <8svskhd@gmail.com>
2024-04-09 15:52:16 +0530

    cart and index

--- src/pageComponents/compare/cartComponent.js
+++ src/pageComponents/compare/cartComponent.js
@@ -6,11 +6,18 @@ const AquaCartComponent = () => {
     <div>
       {cartCount.length > 0 ? (
         <>
+        <div className="row">
           {cartCount.map((r) => (
-            <div key={r}>
-              <AquaCompareCard data={r} />
+            <div className="col" key={r}>
+            <div className="card w-50">
+              <div className="card-body">
+                <h5 className="card-title">{r.title}</h5>
+                <button className="btn btn-dark">Add to Compare</button>
+              </div>
             </div>
+          </div>
           ))}
+          </div>
         </>
       ) : (
         <h4>No Products in Cart yet</h4>
--- src/pageComponents/compare/index.js
+++ src/pageComponents/compare/index.js
@@ -24,7 +24,7 @@ const AquaComparePageComponent = () => {
     },
     {
       title: "Cart",
-      component: <AquaCompareComponent />,
+      component: <AquaCartComponent />,
     },
   ];
 
