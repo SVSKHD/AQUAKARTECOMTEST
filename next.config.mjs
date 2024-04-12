67cf59c41c749dea5f06c22826d2b96cfe1d3574
SVSKHD <hithesh.sunkara@sportsit.net>
2024-04-12 16:47:55 +0530

    env and prod check

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: ["res.cloudinary.com"],
   },
+  env: {
+    NEXT_PUBLIC_LOCAL_URL: "http://localhost:3000",
+    NEXT_PUBLIC_PROD_URL: "https://aquakart.co.in",
+    NEXT_PUBLIC_MERCHANT_ID: "PGTESTPAYUAT",
+    NEXT_PUBLIC_SALT_INDEX: 1,
+    NEXT_PUBLIC_SALT_KEY: "099eb0cd-02cf-4e2a-8aca-3e6c6aff0399",
+    apiKey: "https://aquakart.co.in",
+  },
 };
 
 export default nextConfig;
