914c926db9e24ec2b16e4eeb7a29b4d9bea0fc97
SVSKHD <8svskhd@gmail.com>
2024-05-07 23:39:04 +0530

    order

--- src/pages/api/order/[id].js
+++ src/pages/api/order/[id].js
@@ -15,7 +15,7 @@ router.post(async(req,res)=>{
   console.log(req,req.body)
   const data = req.body
   try {
-    res.json({success:data.success , data:data.data})
+    res.json({success:data.Success , data:data.PAYMENT_METHOD, backup:data})
   } catch (err) {
     res.json({success:false , data:null})
   }
