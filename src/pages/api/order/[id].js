21fee08231ffb18e43121268381a274e068e074b
SVSKHD <hithesh.sunkara@sportsit.net>
2024-05-08 18:08:24 +0530

    removed user id and created the page

--- src/pages/api/order/[id].js
+++ src/pages/api/order/[id].js
@@ -58,7 +58,6 @@ router.post(async (req, res) => {
     const apiResponse = await axios.request(options);
     if (apiResponse.data.success) {
       const orderData = {
-        user: userId,
         totalAmount: apiResponse.data.amount / 100,
         transactionId,
         paymentStatus: "Paid",
