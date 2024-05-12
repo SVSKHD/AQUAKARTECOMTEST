449df76f6b6b7162d666c6e4e3c7704b3d82eb9d
SVSKHD <8svskhd@gmail.com>
2024-05-12 21:16:04 +0530

    keys configurations

--- src/pages/api/pg/phonepe.js
+++ src/pages/api/pg/phonepe.js
@@ -8,7 +8,6 @@ const router = createRouter();
 
 router.post(async (req, res) => {
   const passedPayload = req.body;
-  console.log("phonepe initiated", req.body);
   await db.connectDb(); // Ensure database connection is awaited
 
   const createUserName = (email) => {
@@ -50,7 +49,7 @@ router.post(async (req, res) => {
     const payload = JSON.stringify(data);
     const payloadMain = Buffer.from(payload).toString("base64");
     const keyIndex = 1;
-    const string = payloadMain + "/pg/v1/pay" + process.env.PHONE_PE_KEY;
+    const string = payloadMain + "/pg/v1/pay" + process.env.NEXT_PUBLIC_PHONEPE_KEY;
     const sha256 = crypto.createHash("sha256").update(string).digest("hex");
     const checksum = sha256 + "###" + keyIndex;
 
