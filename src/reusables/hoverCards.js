58c5662d4eb7017f58429ffacca551b8d6511153
SVSKHD <8svskhd@gmail.com>
2024-05-12 21:18:20 +0530

    hover card

--- src/reusables/hoverCards.js
+++ src/reusables/hoverCards.js
@@ -6,7 +6,7 @@ const AquaHoverCards = () => {
           <div className="uni-main">
             <div className="uni-card uni-card1">
               <div className="uni-content">
-                <img src="https://aquakart.co.in/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Faquakartproducts%2Fimage%2Fupload%2Fv1697875761%2Fcategories%2Fhnmzlrucnmpxyirsujx0.png&w=256&q=75" />
+                <img src="https://aquakart.co.in/_next/image?url=https%3A%2F%2Fres.cloudinary.com%2Faquakartproducts%2Fimage%2Fupload%2Fv1697875761%2Fcategories%2Fhnmzlrucnmpxyirsujx0.png&w=256&q=75" alt="Aquakart" />
               </div>
             </div>
             <div className="uni-card uni-card2">
