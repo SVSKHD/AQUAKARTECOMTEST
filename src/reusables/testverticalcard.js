2382d37d87a50e70cfca4f50790f461f82c7c4e0
SVSKHD <8svskhd@gmail.com>
2024-05-12 21:07:18 +0530

    alt fix

--- src/reusables/testverticalcard.js
+++ src/reusables/testverticalcard.js
@@ -45,7 +45,7 @@ const TestVerticalcard = ({ data }) => {
       <div class="hovercard">
         <div class="container1">
           <div class="top">
-            <img src={photos[0].secure_url} className="top" />
+            <img src={photos[0].secure_url} className="top" alt={`Aquakart | ${title}`}/>
           </div>
           <div class={cartAdd ? "bottom clicked" : "bottom"}>
             <div class="left">
