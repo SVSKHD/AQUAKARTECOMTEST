4713f1e1e27a9a70718eb75be86304ba3d723229
SVSKHD <8svskhd@gmail.com>
2024-05-28 18:09:31 +0530

    test vertical card

--- src/reusables/testverticalcard.js
+++ src/reusables/testverticalcard.js
@@ -5,6 +5,8 @@ import AquaCurrencyFormat from "./currencyFormatter";
 import { useRouter } from "next/router";
 import { useSelector } from "react-redux";
 import ProductFunctions from "@/reusableUtils/poroductFunctions";
+import Image from "next/image";
+import AQ from "@/assests/logo.png";
 
 const TestVerticalcard = ({ card }) => {
   const [loading, setLoading] = useState(true);
@@ -13,7 +15,7 @@ const TestVerticalcard = ({ card }) => {
     if (card) {
       setTimeout(() => {
         setLoading(false);
-      }, 2000); // Delay of 1 second
+      }, 6000); // Delay of 1 second
     } else {
       setLoading(true);
     }
@@ -46,71 +48,94 @@ const TestVerticalcard = ({ card }) => {
 
   return (
     <>
-    {loading ?() : (  <div className="hovercard">
-      <div className="container1">
-        <div className="top">
-          {photos && photos.length > 0 ? (
-            <img
-              src={photos[0].secure_url}
-              className="top"
-              alt={`Aquakart | ${title}`}
-            />
-          ) : (
-            <p>No Image Available</p>
-          )}
-        </div>
-        <div className={cartAdd ? "bottom clicked" : "bottom"}>
-          <div className="left">
-            <div className="details">
+      {loading ? (
+        <>
+          <div class="card" aria-hidden="true">
+            <Image src={AQ}  width="250"
+            height="250" className="card-img-top" alt="..." />
+            <div className="card-body">
+              <h5 className="card-title placeholder-glow">
+                <span className="placeholder col-6 rounded-4"></span>
+              </h5>
+              <p className="card-text placeholder-glow">
+                <span className="placeholder col-8 rounded-4"></span>
+                <span className="placeholder col-9 rounded-4"></span>
+                <span className="placeholder col-10 rounded-4"></span>
+                <span className="placeholder col-12 rounded-4"></span>
+              </p>
               <a
-                href="#"
-                className="d-block fw-medium text-dark text-decoration-none"
-                onClick={() => redirectProduct(data._id)}
-              >
-                {title}
-              </a>
-              <AquaCurrencyFormat amount={price} />
-            </div>
-            <div className="buy" onClick={handleAddToCart}>
-              <FaCartArrowDown size={30} />
+                className="btn btn-primary disabled placeholder col-6"
+                aria-disabled="true"
+              ></a>
             </div>
           </div>
-          <div className="right">
-            <div className="done">
-              <FaCheck />
+        </>
+      ) : (
+        <div className="hovercard">
+          <div className="container1">
+            <div className="top">
+              {photos && photos.length > 0 ? (
+                <img
+                  src={photos[0].secure_url}
+                  className="top"
+                  alt={`Aquakart | ${title}`}
+                />
+              ) : (
+                <p>No Image Available</p>
+              )}
             </div>
-            <div className="details">
-              <h6 onClick={() => redirectProduct(data._id)}>{title}</h6>
-              <p>Added to cart</p>
+            <div className={cartAdd ? "bottom clicked" : "bottom"}>
+              <div className="left">
+                <div className="details">
+                  <a
+                    href="#"
+                    className="d-block fw-medium text-dark text-decoration-none"
+                    onClick={() => redirectProduct(data._id)}
+                  >
+                    {title}
+                  </a>
+                  <AquaCurrencyFormat amount={price} />
+                </div>
+                <div className="buy" onClick={handleAddToCart}>
+                  <FaCartArrowDown size={30} />
+                </div>
+              </div>
+              <div className="right">
+                <div className="done">
+                  <FaCheck />
+                </div>
+                <div className="details">
+                  <h6 onClick={() => redirectProduct(data._id)}>{title}</h6>
+                  <p>Added to cart</p>
+                </div>
+                <div
+                  className="remove"
+                  onClick={() => addProductToCart(card, setFavAdd)}
+                >
+                  <MdClose size={30} />
+                </div>
+              </div>
             </div>
-            <div
-              className="remove"
-              onClick={() => addProductToCart(card, setFavAdd)}
-            >
-              <MdClose size={30} />
+          </div>
+          <div className="inside">
+            <div className="icon">
+              {favAdd ? (
+                <FaHeart
+                  onClick={() => addProductToFav(card, setFavAdd)}
+                  size={25}
+                  className="text-light"
+                />
+              ) : (
+                <FaRegHeart
+                  onClick={() => addProductToFav(card, setFavAdd)}
+                  size={25}
+                  className="text-light"
+                />
+              )}
             </div>
           </div>
         </div>
-      </div>
-      <div className="inside">
-        <div className="icon">
-          {favAdd ? (
-            <FaHeart
-              onClick={() => addProductToFav(card, setFavAdd)}
-              size={25}
-              className="text-light"
-            />
-          ) : (
-            <FaRegHeart
-              onClick={() => addProductToFav(card, setFavAdd)}
-              size={25}
-              className="text-light"
-            />
-          )}
-        </div>
-      </div>
-    </div>)}
-  
+      )}
     </>
   );
 };
